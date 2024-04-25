bca6b95aec30315005f3741d191fdfa99b63933a
unknown <2003silvagui@gmail.com>
2024-04-25 00:36:53 -0300

    feat: footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,93 @@
+import { AntDesign } from "@expo/vector-icons";
+import { Box, Button, Center, HStack } from "native-base";
+import { colors } from "../../theme/colors";
+import { useGlobalContext } from "../../context/context";
+
+export default function Footer() {
+  const size = 30;
+  const { selected, navigation } = useGlobalContext();
+
+  return (
+    <>
+      <Center w={"full"} position={"absolute"} bottom={6}>
+        <HStack
+          width={"90%"}
+          h={60}
+          mx={"auto"}
+          rounded={"3xl"}
+          backgroundColor={colors.greenDark}
+          justifyContent={"center"}
+          space={1}
+        >
+          <Button
+            w={"1/6"}
+            h={"full"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            opacity={selected === "calendar" ? 1 : 50}
+            backgroundColor={
+              selected === "calendar"
+                ? colors.greenDarkOpacity
+                : colors.greenDark
+            }
+          >
+            <AntDesign name="calendar" size={size} color="#ffffff" />
+          </Button>
+          <Button
+            w={"1/6"}
+            h={"full"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            opacity={selected === "upload" ? 1 : 50}
+            backgroundColor={
+              selected === "upload" ? colors.greenDarkOpacity : colors.greenDark
+            }
+          >
+            <AntDesign name="upload" size={size} color="#ffffff" />
+          </Button>
+          <Button
+            w={"1/6"}
+            h={"full"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            opacity={selected === "creditcard" ? 1 : 50}
+            backgroundColor={
+              selected === "creditcard"
+                ? colors.greenDarkOpacity
+                : colors.greenDark
+            }
+          >
+            <AntDesign name="creditcard" size={size} color="#ffffff" />
+          </Button>
+          <Button
+            w={"1/6"}
+            h={"full"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            opacity={selected === "user" ? 1 : 50}
+            backgroundColor={
+              selected === "user" ? colors.greenDarkOpacity : colors.greenDark
+            }
+          >
+            <AntDesign name="user" size={size} color="#ffffff" />
+          </Button>
+          <Button
+          onPress={() => navigation("setting")}
+            w={"1/6"}
+            h={"full"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            opacity={selected === "setting" ? 1 : 50}
+            backgroundColor={
+              selected === "setting"
+                ? colors.greenDarkOpacity
+                : colors.greenDark
+            }
+          >
+            <AntDesign name="setting" size={size} color="#ffffff" />
+          </Button>
+        </HStack>
+      </Center>
+    </>
+  );
+}
