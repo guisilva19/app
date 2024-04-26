d9d10c053163fe92db7a9ef3a64e62492f1b4db1
unknown <2003silvagui@gmail.com>
2024-04-26 14:17:15 -0300

    feat: calendar screen

---
+++ src/app/(auth)/calendar/index.tsx
@@ -0,0 +1,111 @@
+import {
+  Box,
+  Button,
+  Center,
+  HStack,
+  Heading,
+  Modal,
+  Text,
+  VStack,
+} from "native-base";
+import { colors } from "../../../theme/colors";
+import React, { useState } from "react";
+
+import { ScrollView } from "react-native";
+import {
+  AntDesign,
+  FontAwesome,
+  MaterialIcons,
+  MaterialCommunityIcons,
+  Feather,
+} from "@expo/vector-icons";
+import Footer from "../../../components/Footer/Footer";
+import { useGlobalContext } from "../../../context/context";
+import { redirectWhatsApp } from "../../../utils/whatsapp/redirect";
+
+export default function Calendar() {
+  return (
+    <>
+      <ScrollView style={{ backgroundColor: colors.background }}>
+        <Center
+          w="100%"
+          h="100%"
+          pb={100}
+          style={{ backgroundColor: colors.background }}
+        >
+          <VStack
+            backgroundColor={colors.background}
+            h="full"
+            w="full"
+            maxW={350}
+            mt={20}
+          >
+            <Heading fontFamily="PathwayBold" fontSize={32}>
+              Acompanhamento dos eventos
+            </Heading>
+            <Text fontFamily="PathwayRegular" fontSize={16} mt={2}>
+              Acompanhe abaixo as etapas agendadas
+            </Text>
+
+            <Box
+              backgroundColor={"white"}
+              width={"20"}
+              mt={6}
+              justifyContent={"center"}
+              borderTopRadius={"lg"}
+              py={"1.5"}
+            >
+              <Text textAlign={"center"} fontFamily="PathwayBold">
+                Você
+              </Text>
+            </Box>
+            <VStack
+              backgroundColor={"white"}
+              borderTopRightRadius={"lg"}
+              borderBottomLeftRadius={"lg"}
+              borderBottomRightRadius={"lg"}
+            >
+              <ComponentAgend />
+            </VStack>
+          </VStack>
+        </Center>
+      </ScrollView>
+      <Footer />
+    </>
+  );
+}
+
+const ComponentAgend = () => {
+  return (
+    <>
+      <HStack
+        backgroundColor={"white"}
+        h={20}
+        p={4}
+        rounded={"lg"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Box
+          backgroundColor={colors.greenDark}
+          h={"full"}
+          rounded={"sm"}
+          w={"12"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Text color={"white"} fontFamily={"PathwayBold"}>
+            11
+          </Text>
+          <Text color={"white"} fontFamily={"PathwayBold"}>
+            JUN
+          </Text>
+        </Box>
+        <Text fontFamily={"PathwayRegular"} fontSize={16} w={"65%"}>
+          Cadastro no aplicativo
+        </Text>
+        <AntDesign name="checkcircleo" size={24} color="black" />
+      </HStack>
+    </>
+  );
+};
